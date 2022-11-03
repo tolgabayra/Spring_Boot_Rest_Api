96441a4e0af8e4a7b60e9802a16a7d559251903c
Tolga BAYRAK <tolga@pop-os.localdomain>
2022-11-03 10:17:40 +0300

    Api

--- src/main/java/com/tolgabayrak/springserver/controller/UserController.java
+++ src/main/java/com/tolgabayrak/springserver/controller/UserController.java
@@ -1,13 +1,28 @@
 package com.tolgabayrak.springserver.controller;
 
+import com.tolgabayrak.springserver.model.User;
 import com.tolgabayrak.springserver.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.apache.coyote.Request;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("v1/users")
 public class UserController {
-    @Autowired
-    UserService userService;
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public User createUser(@RequestBody User user){
+        return ResponseEntity.ok(userService.createUser(user)).getBody();
+    }
+
+
+
 }
--- src/main/java/com/tolgabayrak/springserver/service/UserService.java
+++ src/main/java/com/tolgabayrak/springserver/service/UserService.java
@@ -9,7 +9,7 @@ import java.util.Optional;
 @Service
 public class UserService {
 
-    private final UserRepository userRepository;
+    private UserRepository userRepository;
 
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
